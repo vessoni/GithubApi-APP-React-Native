e6c83126f03dc0fd3925ebc93f6c59a4f9552962
Alexandre Almeida Vessoni <av.vessoni@gmail.com>
2019-09-06 12:17:23 -0300

    config prettier

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Hello, world! </Text>
     </View>
   );
