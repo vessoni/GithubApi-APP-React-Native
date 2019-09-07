df7c280a185a0b215e33c47e4a0d9f84450b1dcd
= <av.vessoni@gmail.com>
2019-09-07 01:31:17 -0300

    Add ReactorTron

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import './config/ReactotronConfig';
+
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello, world!! </Text>
+      <Text>Hello, aaaa world!! aa</Text>
     </View>
   );
 }
