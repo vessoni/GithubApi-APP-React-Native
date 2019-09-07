f71b72085d44bf7061010250f9f09e4a4a9a8724
= <av.vessoni@gmail.com>
2019-09-07 01:03:42 -0300

    Some modification in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,9 @@ module.exports = {
         extensions: ['.jsx', '.js']
       }
     ],
-    'import/prefer-default-export' : 'off'
+    'import/prefer-default-export' : 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello, world! </Text>
+      <Text>Hello, world!! </Text>
     </View>
   );
 }
