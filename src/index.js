afd434af4f0ae620fd96ff0ff089e105cf870052
= <av.vessoni@gmail.com>
2019-09-08 15:37:26 -0300

    Add ReacNavigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello, aaaa world!! aa</Text>
-    </View>
-  );
+  return <Routes />;
 }
